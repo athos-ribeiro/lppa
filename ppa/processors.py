c7db3a1d8ac8fca93345715c592661ff3aab1d3d
Athos Ribeiro <athos.ribeiro@canonical.com>
2021-05-19 19:32:57 -0300

    Remove unused typing imports
    Signed-off-by: Athos Ribeiro <athos.ribeiro@canonical.com>

--- ppa/processors.py
+++ ppa/processors.py
@@ -15,7 +15,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import logging
-from typing import List, Type
 
 from lazr.restfulclient.errors import NotFound
 
