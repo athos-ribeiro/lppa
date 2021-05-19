df44792beec51e7f940bb25411bc5824cdf10bac
Athos Ribeiro <athos.ribeiro@canonical.com>
2021-05-19 17:39:43 -0300

    Improve auth module documentation
    Signed-off-by: Athos Ribeiro <athos.ribeiro@canonical.com>

--- ppa/auth.py
+++ ppa/auth.py
@@ -1,21 +1,35 @@
 from launchpadlib.launchpad import Launchpad
 
+
 API_VERSION = 'devel'
 APP_NAME = 'ppa'
-# LP_ENV = 'production'
-LP_ENV = 'staging'
+LP_ENV = 'production'  # or staging
 
 
 class AuthenticationError(Exception):
-    pass
+    """Error for authentication failures"""
 
 
 class Session():
+    """Launchpad session interface"""
     def __init__(self, anonymous=False, lp_env=LP_ENV):
+        """Initializer
+
+        param anonymous: bool, whether to use an anonimous session
+        param lp_env: string, launchpad environment to pass to launchpadlib
+        """
         self.anonymous = anonymous
         self.lp_env = lp_env
 
     def _get_authenticated_session(self, anonymous_fallback):
+        """Retrieve a new launchpad authenticated session
+
+        param anonymous_fallback: bool, whether to fallback to an anonimous session on
+            authentication failures
+        raises AithenticationError: Failed to authenticate without anonymous fallbacks enabled
+        return: Launchpad session
+        rtype: launchpadlib.launchpad.Launchpad
+        """
         try:
             session = Launchpad.login_with(
                 APP_NAME,
@@ -32,12 +46,24 @@ class Session():
         return session
 
     def _get_anonymous_session(self):
+        """Retrieve a new anonymous launchpad session
+
+        return: Launchpad session
+        rtype: launchpadlib.launchpad.Launchpad
+        """
         return Launchpad.login_anonymously(APP_NAME, self.lp_env, version=API_VERSION)
 
     def _no_auth_failure(self):
         raise AuthenticationError(f"Could not Authenticate with Launchpad in '{self.lp_env}'")
 
     def get_session(self, anonymous_fallback=False):
+        """Retrieve a new launchpad session
+
+        param anonymous_fallback: bool, whether to fallback to an anonimous session on
+            authentication failures
+        return: Launchpad session
+        rtype: launchpadlib.launchpad.Launchpad
+        """
         if self.anonymous:
             return self._get_anonymous_session()
 
